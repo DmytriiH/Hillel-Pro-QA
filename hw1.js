fafd532516e8d631398453ed2717fc4dccb376ef
DmytriiH <122825601+DmytriiH@users.noreply.github.com>
2024-01-25 23:41:25 +0200

    Update hw1.js

--- hw1.js
+++ hw1.js
@@ -7,12 +7,11 @@
 // Не використовувати isNaN/ Number.isNaN
 
 
-// var a = NaN;
-
-// if (a === a) {
-//   console.log("Значення є NaN");
-//   console.log(typeof(a));
-// } else {
-//   console.log("Значення не є NaN");
-//   console.log(typeof(a));  
+// function myIsNaN(value) {
+//   return value !== value;
 // }
+// console.log(myIsNaN(NaN));        
+// console.log(myIsNaN(123));        
+// console.log(myIsNaN("abc"));     
+// console.log(myIsNaN(undefined));  
+// console.log(myIsNaN("123")); 
